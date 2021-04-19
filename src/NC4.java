51e222ebdc809547edb472b2013e7f9206369fac
Deer <1179727810@qq.com>
2021-04-19 11:11:31 +0800

    牛客top200

--- src/NC4.java
+++ src/NC4.java
@@ -20,4 +20,18 @@ public class NC4 {
         }
         return false;
     }
+
+    public boolean hasCycle2(ListNode head) {
+        if (head==null||head.next==null) return false;
+        ListNode slow=head;
+        ListNode fast=head.next;
+        while (slow!=fast){
+            if (fast==null||fast.next==null){
+                return false;
+            }
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        return true;
+    }
 }
